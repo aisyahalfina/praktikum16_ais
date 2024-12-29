3bddace8fff69b5cd9135082a74118ac4ce71729
aisyahalfina <aisyahalfina04@gmail.com>
2024-12-29 08:05:09 +0700

    Add files via upload

---
+++ storage/framework/views/bef1815bf367d2b09281c4c5cbafdd86.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo e($pageTitle ?? 'Data Master'); ?></title>
+    <?php echo app('Illuminate\Foundation\Vite')('resources/sass/app.scss'); ?>
+</head>
+
+<body>
+    <?php echo $__env->make('layouts.nav', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+    <?php echo $__env->yieldContent('content'); ?>
+    <?php echo app('Illuminate\Foundation\Vite')('resources/js/app.js'); ?>
+</body>
+
+</html>
+<?php /**PATH C:\Users\LENOVO\Documents\Kuliah\Semester 3\Pengembangan Aplikasi Website\File Project\Semester 3\Laravel baru\Praktikum15_Tugas\ais_prak_15_auth\resources\views/layouts/app.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/cdbd7521f4118ff78e4ce27f826626b7.php
@@ -0,0 +1,14 @@
+<div class="container mt-4">
+    <h4 style="font-weight: bolder"><?php echo e($pageTitle); ?></h4>
+    <hr>
+    <div class="d-flex align-items-center py-2 px-4 bg-light rounded-3 border">
+        <!-- Kondisional untuk menentukan ikon berdasarkan $pageTitle -->
+        <div
+            class="<?php if($pageTitle === 'Home'): ?> bi-house-fill
+                    <?php elseif($pageTitle === 'Profile'): ?> bi-person-circle
+                    <?php else: ?> bi-file-earmark <?php endif; ?> me-3 fs-1">
+        </div>
+        <h4 class="mb-0">Well done! this is <?php echo e($pageTitle); ?>.</h4>
+    </div>
+</div>
+<?php /**PATH C:\Users\LENOVO\Documents\Kuliah\Semester 3\Pengembangan Aplikasi Website\File Project\Semester 3\Laravel baru\Praktikum16\ais_prak_16\resources\views/default.blade.php ENDPATH**/ ?>
